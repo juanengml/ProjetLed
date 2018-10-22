e1f6c87c9d2899fd55a0211c3617e889155b1594
juanengml <juanengml@gmail.com>
2018-10-22 16:33:21 -0300

    Update besteiras"

--- v2/app.py
+++ v2/app.py
@@ -5,34 +5,41 @@ from flask import render_template, request,url_for
  
 app = Flask(__name__, static_url_path='/static')
 
-client = mqtt.Client()
+#client = mqtt.Client()
 # conecta no broker
-client.connect("192.168.100.10", 1883)
+#client.connect("192.168.100.10", 1883)
 
 
  
 @app.route("/")
-
 def index():
-    return render_template('index.html')
+    file = open("data.txt","r").read().split("\n")[0].split(" ")[1]
+    data = {"lampada",file}
+    return render_template('index.html',data=data)
 
 @app.route('/led1-ON')
 def left_side():
-   data1="led1-ON"
-   print data1
-   client.publish("/led1/","1")
+   file = open("data.txt","w")
+   print "led1-ON" 
+ #  client.publish("/led1/","1")
+    
+   file.write("Lampada 1")
+   file.close()
+   return "True"
 
-   return 'true'
  
 @app.route('/led1-OFF')
 def right_side():
-   data1="led1-OFF"
-   print data1
-   client.publish("/led1/","0")
-   return 'true'
+   file = open("data.txt","w")
+   print "led1-OFF"
+   # client.publish("/led1/","0")
+   file.write("Lampada 0")
+   file.close() 
+   return "false"
  
 if __name__ == "__main__":
  print "Start"
+
  app.run(host='0.0.0.0',port=80,debug=True)
 
 
